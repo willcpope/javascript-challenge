48e67f266bc19a6b51fc1740997d2ce94ed0f875
Will Pope <57363781+willcpope@users.noreply.github.com>
2020-03-24 14:18:11 -0400

    Add files via upload

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,26 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+function createTable(elements) {
+    var tbody = d3.select("tbody");    
+    tbody.html("");
+    elements.forEach((ufo_data) => {
+        var row = tbody.append("tr");
+        Object.entries(ufo_data).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+}
+
+var button = d3.select("#filter-btn");
+
+button.on("click", function() {
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+    var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+    createTable(filteredData);
+
+});
+
+createTable(tableData);
